8bb83933ccd49a4e8595fbe6aa71032fc2ebd386
ricardopetrere <ricardo19.93@hotmail.com>
2014-05-24 16:47:19 -0300

    AAA

--- src/java/com/hasz/model/Cliente.java
+++ src/java/com/hasz/model/Cliente.java
@@ -3,60 +3,67 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.hasz.model;
 
 import java.io.Serializable;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
-import javax.faces.bean.ManagedBean;
 import javax.persistence.*;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
+
 /**
  *
  * @author Admin
  */
 @Entity
 @Table(name = "Cliente")
-public class Cliente implements Serializable{
+public class Cliente implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idCliente")
     private int idCliente;
-    
+
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "idEndereco",insertable = true,updatable = true)
+    @JoinColumn(name = "idEndereco", insertable = true, updatable = true)
     @Fetch(FetchMode.JOIN)
     @Cascade(CascadeType.SAVE_UPDATE)
     private Endereco endereco;
-    
-    @Column(length = 80)
+
+    @Size(min = 1, max = 100)
+    @Column(length = 100)
     private String nome;
-    
+
     @Temporal(TemporalType.DATE)
     private Date dataCadastro;
-    
+
     @Temporal(TemporalType.DATE)
+    @Past
     private Date dataNascimento;
-    
+
     private char sexo;
-    
-    private String estadoCivil;
-    
-    @OneToMany(mappedBy = "cliente",fetch = FetchType.LAZY)
+
+    @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
     @Cascade(CascadeType.SAVE_UPDATE)
     private Set<Reserva> reserva = new HashSet<Reserva>();
-    
-    @OneToMany(mappedBy = "cliente",fetch = FetchType.LAZY)
+
+    @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
     @Cascade(CascadeType.SAVE_UPDATE)
     private Set<Historico> historico = new HashSet<Historico>();
-    
+
     @Column(length = 14)
     private String cpf;
+
+    @Size(min = 6, max = 20)
+    @Column(length = 25)
+    private String senha;
+
     /**
      * @return the idCliente
      */
@@ -142,20 +149,6 @@ public class Cliente implements Serializable{
     }
 
     /**
-     * @return the estadoCivil
-     */
-    public String getEstadoCivil() {
-        return estadoCivil;
-    }
-
-    /**
-     * @param estadoCivil the estadoCivil to set
-     */
-    public void setEstadoCivil(String estadoCivil) {
-        this.estadoCivil = estadoCivil;
-    }
-
-    /**
      * @return the reserva
      */
     public Set<Reserva> getReserva() {
@@ -195,5 +188,19 @@ public class Cliente implements Serializable{
      */
     public void setCpf(String cpf) {
         this.cpf = cpf;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
 }
