b62fc12a3646f745fde7fae98788f4bf461df5bd
ricardopetrere <ricardo19.93@hotmail.com>
2014-05-24 00:50:11 -0300

    24_05

---
+++ src/java/com/hasz/bean/dao/ClienteDAO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.hasz.bean.dao;
+import com.hasz.bean.Cliente;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author Ricardo
+ */
+@ManagedBean
+public class ClienteDAO {
+    /**
+     * 
+     * @param login
+     * @param senha
+     * @return 
+     */
+    public boolean validaLogin(String login,String senha){
+       //if((login.compareTo(login)==0)&&(senha.compareToIgnoreCase(senha)==0))
+               return false;
+    }
+}
---
+++ src/java/com/hasz/bean/dao/HotelDAO.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hasz.bean.dao;
+import com.hasz.bean.Hotel;
+
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author Ricardo
+ */
+@ManagedBean
+public class HotelDAO {
+    /**
+     * 
+     * @return 
+     */
+    public List<Hotel> listaHoteis()
+    {
+        return null;
+    }
+}
